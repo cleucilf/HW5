21fb30fb96251f52c1fd8409446d55a9a850bd61
Cleucil Francisco <cleucilf@gmail.com>
2026-02-25 23:39:15 -0800

    Add overflow and underflow

--- test_calculator.c
+++ test_calculator.c
@@ -7,13 +7,22 @@ void test_add_basic(void){
     TEST_ASSERT_EQUAL(5, add(2, 3));
 }
 void test_add_positive_negative(void){
-    TEST_ASSERT_EQUAL(-2, add(2, -4));
+    TEST_ASSERT_EQUAL(1, add(5, -4));
 }
 void test_add_negative_negative(void){
     TEST_ASSERT_EQUAL(-7, add(-3, -4));
 }
 void test_add_zero(void){
+    TEST_ASSERT_EQUAL(10, add(10, 0));
     TEST_ASSERT_EQUAL(0, add(0, 0));
+}
+void test_add_overflow(void){
+    int result = add(INT_MAX, 1);
+    TEST_ASSERT_TRUE(result < 0);
+}
+void test_add_underflow(void){
+    int result = add(INT_MIN, -1);
+    TEST_ASSERT_TRUE(result > 0);
 }
 int main(void){
     UNITY_BEGIN();
@@ -21,5 +30,7 @@ int main(void){
     RUN_TEST(test_add_positive_negative);
     RUN_TEST(test_add_negative_negative);
     RUN_TEST(test_add_zero);
+    RUN_TEST(test_add_overflow);
+    RUN_TEST(test_add_underflow);
     return UNITY_END();
 }
