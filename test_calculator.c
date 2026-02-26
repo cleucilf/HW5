c2208afd1e858fd837daf5aef7c3745fa10f6816
Cleucil Francisco <cleucilf@gmail.com>
2026-02-25 23:27:43 -0800

    Add more unit tests

--- test_calculator.c
+++ test_calculator.c
@@ -6,8 +6,20 @@ void tearDown(void){}
 void test_add_basic(void){
     TEST_ASSERT_EQUAL(5, add(2, 3));
 }
+void test_add_positive_negative(void){
+    TEST_ASSERT_EQUAL(-2, add(2, -4));
+}
+void test_add_negative_negative(void){
+    TEST_ASSERT_EQUAL(-7, add(-3, -4));
+}
+void test_add_zero(void){
+    TEST_ASSERT_EQUAL(0, add(0, 0));
+}
 int main(void){
     UNITY_BEGIN();
     RUN_TEST(test_add_basic);
+    RUN_TEST(test_add_positive_negative);
+    RUN_TEST(test_add_negative_negative);
+    RUN_TEST(test_add_zero);
     return UNITY_END();
 }
